f522b241d21505d404d85ac58ef20875d5770f78
Gilbert Hb <150328032+Gilberthb@users.noreply.github.com>
2024-10-20 15:46:37 +0200

    Add files via upload

---
+++ src/components/SideMenu.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function SideMenu() {
+  const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <div className="h-full flex-col justify-between  bg-white hidden lg:flex ">
+      <div className="px-4 py-6">
+        <nav aria-label="Main Nav" className="mt-6 flex flex-col space-y-1">
+         
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+              <Link to="/inventory">
+                <div className="flex items-center gap-2">
+                  <img
+                    alt="inventory-icon"
+                    src={require("../assets/inventory-icon.png")}
+                  />
+                  <span className="text-sm font-medium"> Product </span>
+                </div>
+              </Link>
+            </summary>
+          </details>
+
+        
+          <Link
+            to="/sales"
+            className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+          >
+            <img alt="sale-icon" src={require("../assets/supplier-icon.png")} />
+            <span className="text-sm font-medium"> order</span>
+          </Link>
+
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+             
+            </summary>
+          </details>
+        </nav>
+      </div>
+
+    </div>
+  );
+}
+
+export default SideMenu;
